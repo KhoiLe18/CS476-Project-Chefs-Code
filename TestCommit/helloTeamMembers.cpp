d13e1789c247402b591c538e78a0b906a9025e83
mohammadakibhossain <mohammadakibhossain@gmail.com>
2025-02-03 16:02:20 -0600

    Update helloTeamMembers.cpp

--- TestCommit/helloTeamMembers.cpp
+++ TestCommit/helloTeamMembers.cpp
@@ -9,6 +9,7 @@ int main() {
 	member2 = "Alia Sayeed :)";
 	member3 = "Nairit Patel";
 	member4 = "Kavya Parakh";
+	member5 = "Mohammad Akib";
 	string membersList[6] = {member1, member2, member3, member4, member5, member6};
 
 	for (int i = 0; i < 6; i++) {
