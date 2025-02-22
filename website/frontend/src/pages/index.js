3be499e72ae3451413283b567f5b6f523d3577c5
AJ-Say <157613014+AJ-Say@users.noreply.github.com>
2025-02-22 13:08:15 -0600

    frontend/backend work

--- website/frontend/src/pages/index.js
+++ website/frontend/src/pages/index.js
@@ -11,14 +11,14 @@ app.get('/', (request, res) => {res.render('index')});
 
 app.post('/search', async(req, res) => {
     const {query} = req.body
-    const response = await axios.get(https://api.spoonacular.com/recipes/complexSearch?query=${query}&apiKey=${api_Key})
+    const response = await axios.get(`https://api.spoonacular.com/recipes/complexSearch?query=${query}&apiKey=${api_Key}`)
     const recipes = response.data.results;
     res.render('results', {recipes});
 })
 
 app.get('/recipe/:id', async(req, res) => {
     const {id} = req.params;
-    const response = await axios.get(https://api.spoonacular.com/recipes/${id}/information?apiKey=${api_Key})
+    const response = await axios.get(`https://api.spoonacular.com/recipes/${id}/information?apiKey=${api_Key}`)
     const recipe = response.data;
     res.render('recipe', {recipe})
 
