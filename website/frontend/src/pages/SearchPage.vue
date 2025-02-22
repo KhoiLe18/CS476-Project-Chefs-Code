aea31ef8195d855113b800b0a895628d57d272cc
kavya24123 <kavyaparakh24@gmail.com>
2025-02-22 17:01:43 -0600

    new homepage

--- website/frontend/src/pages/SearchPage.vue
+++ website/frontend/src/pages/SearchPage.vue
@@ -61,7 +61,7 @@
 </template>
    
 <script setup>
-
+import ".//js/search.js";
 import { ref } from "vue";
     const selectedCuisines = ref([]);
     const cuisines = ref([
@@ -111,6 +111,6 @@ import { ref } from "vue";
    
 </script>
    
-   
+  
 <style>
 </style>
