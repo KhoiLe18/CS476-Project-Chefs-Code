6ae082e5853a5b74b26eccc6f36a56e0e8161b9c
KhoiLe18 <letankhoiedu@gmail.com>
2025-02-24 16:43:03 -0600

    Delete Test Commit

--- TestCommit/AkibsPart/login/eventRegisterLogin.js
+++
@@ -1,5 +0,0 @@
-//  Add code to register a "submit" event on the login form, 
-//  and assign an event handler.
-//  Hint: use addEventListener() to register events.
-
-document.getElementById("login").addEventListener("submit", validateLogin);
