2dc507f597a53b5a469e93ad93e680c5d9979b3e
AJ-Say <157613014+AJ-Say@users.noreply.github.com>
2025-03-07 12:15:15 -0600

    Update db.js

--- website/backend/model/db.js
+++ website/backend/model/db.js
@@ -1,7 +1,7 @@
 const mariadb = require('mariadb');
 const pool = mariadb.createPool({
     host: 'fd7a:115c:a1e0::3401:9a65',
-    user: 'chefscode',      
+    user: 'remote',      
     password: 'ChefsCode_476',
     database: 'chefscode',
     connectionLimit: 5
