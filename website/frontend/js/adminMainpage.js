85f7add5416dcb2c691ee48f516d9fde7c8a2af4
AJ-Say <157613014+AJ-Say@users.noreply.github.com>
2025-03-09 12:46:42 -0600

    cleaning up adminMainpage

---
+++ website/frontend/js/adminMainpage.js
@@ -0,0 +1,7 @@
+const logoutBtn = document.getElementById('logout-btn');
+const usersTableBody = document.getElementById('users-table-body');
+const userSearch = document.getElementById('user-search');
+const searchBtn = document.getElementById('search-btn');
+const deleteModal = document.getElementById('delete-modal');
+const confirmDeleteBtn = document.getElementById('confirm-delete-btn');
+const cancelDeleteBtn = document.getElementById('cancel-delete-btn');
