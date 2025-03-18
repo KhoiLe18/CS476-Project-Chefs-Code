aa3db3448ea497002c967c7ada73460953c6618c
AJ-Say <157613014+AJ-Say@users.noreply.github.com>
2025-03-18 13:13:40 -0600

    fixing user deletion

--- website/backend/controller/index.js
+++ website/backend/controller/index.js
@@ -153,9 +153,9 @@ app.delete('/api/users/:userId', async (req, res) => {
         const conn = await pool.getConnection();
         
         // First get the user information before deleting
-        const userQuery = 'SELECT first_name, last_name FROM Users WHERE user_id = ?';
+        const userQuery = 'SELECT first_name, last_name, user_id FROM Users WHERE user_id = ?';
         const userInfo = await conn.query(userQuery, [userId]);
-        //console.log("USER INFO IS ", userInfo);
+        console.log("USER INFO IS ", userInfo);
         
         if (userInfo.length === 0) {
             conn.release();
@@ -164,8 +164,14 @@ app.delete('/api/users/:userId', async (req, res) => {
         
         // Store user info
         const fullName = `${userInfo[0].first_name} ${userInfo[0].last_name}`;
+        const userid = userInfo[0].user_id;
+        //console.log(userid, " is the user id!!!");
+
+        //delete the user's favourited recipes from the favourites table
+        const deleteFavouritesQuery = 'DELETE FROM favourites WHERE user_id = ?';
+        const results = await conn.query(deleteFavouritesQuery, [userId]);
         
-        // Delete the user
+        // Once the favourites associated with this user are gone, we can delete the user
         const deleteQuery = 'DELETE FROM Users WHERE user_id = ?';
         const result = await conn.query(deleteQuery, [userId]);
         conn.release();
